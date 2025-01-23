84c05a86f280299aa2c5eb8ef2c801fa49ab72ce
Mike Balogun <mxbalogun@gmail.com>
2025-01-23 00:19:41 -0500

    Created new project and established conn string. Figuring out how to protect password

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/BankifyDB", "root", "");
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from user_accounts");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("username"));
+                System.out.println(resultSet.getString("password"));
+            }
+        } catch (Exception e){
+           System.out.println(e);
+        }
+
+    }
+}
